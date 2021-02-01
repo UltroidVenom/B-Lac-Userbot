d0a467a71530e1b4fc25196e52bb54abe6cda393
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-02-01 18:56:45 +0530

    Update animation3.py

--- userbot/plugins/animation3.py
+++ userbot/plugins/animation3.py
@@ -62,32 +62,6 @@ async def _(event):
         deq.rotate(1)
 
 
-@borg.on(admin_cmd(pattern=r"deploy$"))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 3
-    animation_ttl = range(0, 12)
-    await event.edit("Deploying...")
-    animation_chars = [
-        "**Heroku Connecting To Latest Github Build **",
-        f"**Build started by user** ** {DEFAULTUSER} **",
-        f"**Deploy** `535a74f0` **by user** ** {DEFAULTUSER} **",
-        "**Restarting Heroku Server...**",
-        "**State changed from up to starting**",
-        "**Stopping all processes with SIGTERM**",
-        "**Process exited with** `status 143`",
-        "**Starting process with command** `python3 -m stdborg`",
-        "**State changed from starting to up**",
-        "__INFO:Userbot:Logged in as 557667062__",
-        "__INFO:Userbot:Successfully loaded all plugins__",
-        "**Build Succeeded**",
-    ]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 12])
-
-
 @borg.on(admin_cmd(pattern="dump ?(.*)"))
 async def _(message):
     try:
