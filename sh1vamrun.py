6f38c0a86ba5990fc910ec3b1f6c75e78888b2d2
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2021-03-13 22:37:40 +0530

    Create sh1vamrun.py

---
+++ sh1vamrun.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/B-Lac/B-Lac-Userbot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
