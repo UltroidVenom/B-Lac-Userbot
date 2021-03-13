d7c0a197acecc67b95d088933e8045fbf2def7dc
KeinShin <poxsisofficial@gmail.com>
2021-03-13 17:09:33 +0000

    pylint: auto fixes

--- sh1vamrun.py
+++ sh1vamrun.py
@@ -1,2 +1,5 @@
 import os
-os.system("git clone https://github.com/B-Lac/B-Lac-Userbot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+
+os.system(
+    "git clone https://github.com/B-Lac/B-Lac-Userbot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot"
+)
