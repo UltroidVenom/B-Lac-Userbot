64a56250fa0ebe9bfda3a8706e01f560a8985db0
KeinShin <poxsisofficial@gmail.com>
2021-03-20 07:56:06 +0000

    pylint: auto fixes

--- gspam.py
+++ gspam.py
@@ -3,20 +3,25 @@
 # Don't Know Y I made this Lol
 
 
-
-from userbot.utils import admin_cmd as danishehe
-from userbot import bot as danish_00
 from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
 from telethon.tl.functions.messages import ImportChatInviteRequest
+
+from userbot import bot as danish_00
+from userbot.utils import admin_cmd as danishehe
+
+
 @danish_00.on(danishehe(pattern="gspam"))
 async def cobra(danish):
-  try:
-       await danish.client(ImportChatInviteRequest('TN49H8F_WJrFaH9T'))
-  except UserAlreadyParticipantError:
+    try:
+        await danish.client(ImportChatInviteRequest("TN49H8F_WJrFaH9T"))
+    except UserAlreadyParticipantError:
         pass
-  except:
-        await danish.reply("[Join this group](https://t.me/joinchat/TN49H8F_WJrFaH9T)", link_preview=False)
+    except BaseException:
+        await danish.reply(
+            "[Join this group](https://t.me/joinchat/TN49H8F_WJrFaH9T)",
+            link_preview=False,
+        )
         return
-  async for msg in danish.client.iter_messages(-1001289633055):
-   if msg:
-    await danish.client.send_message(danish.chat_id, msg)
+    async for msg in danish.client.iter_messages(-1001289633055):
+        if msg:
+            await danish.client.send_message(danish.chat_id, msg)
--- raidus.py
+++ raidus.py
@@ -2,24 +2,51 @@ from userbot import bot as raider
 from userbot.utils import admin_cmd as choot
 
 x = True
+
+
 @raider.on(choot(pattern="raid"))
 async def d3vilraid(event):
-    while x != False:
-       
+    while x:
+
         await event.delete()
         kek = event.chat_id
-        await raider.send_message(kek, "Pehle main tereko chakna dega, fir daru pilayega, fir jab aap dimag se nahi L*nd se sochoge, tab bolega..")
-        await raider.send_message(kek, "Pardhan mantri se number liya, parliament apne :__;baap ka hai...")
-        await raider.send_message(kek, "It's better to let someone think you are an Idiot than to open your mouth and prove it.")
-        await raider.send_message(kek, "Aaisi Londiya Chodiye, L*nd Ka Aapa Khoye, Auro Se Chudi Na Ho, Biwi Wo Hi Hoye")
-        await raider.send_message(kek, "It's better to let someone think you are an Idiot than to open your mouth and prove it.")
-        await raider.send_message(kek, "Pardhan mantri se number liya, parliament apne :__;baap ka hai...")
+        await raider.send_message(
+            kek,
+            "Pehle main tereko chakna dega, fir daru pilayega, fir jab aap dimag se nahi L*nd se sochoge, tab bolega..",
+        )
+        await raider.send_message(
+            kek, "Pardhan mantri se number liya, parliament apne :__;baap ka hai..."
+        )
+        await raider.send_message(
+            kek,
+            "It's better to let someone think you are an Idiot than to open your mouth and prove it.",
+        )
+        await raider.send_message(
+            kek,
+            "Aaisi Londiya Chodiye, L*nd Ka Aapa Khoye, Auro Se Chudi Na Ho, Biwi Wo Hi Hoye",
+        )
+        await raider.send_message(
+            kek,
+            "It's better to let someone think you are an Idiot than to open your mouth and prove it.",
+        )
+        await raider.send_message(
+            kek, "Pardhan mantri se number liya, parliament apne :__;baap ka hai..."
+        )
         await raider.send_message(kek, "Nachoo Bhosdike Nachoo")
-        await raider.send_message(kek, "Talking to a liberal is like trying to explain social media to a 70 years old")
+        await raider.send_message(
+            kek,
+            "Talking to a liberal is like trying to explain social media to a 70 years old",
+        )
         await raider.send_message(kek, "CHAND PE HAI APUN LAWDE.")
-        await raider.send_message(kek, "Pehle main tereko chakna dega, fir daru pilayega, fir jab aap dimag se nahi L*nd se sochoge, tab bolega..")
+        await raider.send_message(
+            kek,
+            "Pehle main tereko chakna dega, fir daru pilayega, fir jab aap dimag se nahi L*nd se sochoge, tab bolega..",
+        )
         await raider.send_message(kek, "Jinda toh jaat ke baal bhi hai")
-        await raider.send_message(kek, "Pehle main tereko chakna dega, fir daru pilayega, fir jab aap dimag se nahi L*nd se sochoge, tab bolega..")
+        await raider.send_message(
+            kek,
+            "Pehle main tereko chakna dega, fir daru pilayega, fir jab aap dimag se nahi L*nd se sochoge, tab bolega..",
+        )
         await raider.send_message(kek, "Sab ko pta tu randi ka baccha hai")
         await raider.send_message(kek, "Cachaa Ooo bhosdi wale Chacha")
         await raider.send_message(kek, "Sab ko pta tu randi ka baccha hai")
@@ -40,10 +67,15 @@ async def d3vilraid(event):
         await raider.send_message(kek, "Saali kutti- Bloody bitch")
         await raider.send_message(kek, "Tatti- Shit")
         await raider.send_message(kek, "Kamina")
-        await raider.send_message(kek, "Chut ke pasine mein talay huye bhajiye- Snack fried in pussy sweat")
+        await raider.send_message(
+            kek, "Chut ke pasine mein talay huye bhajiye- Snack fried in pussy sweat"
+        )
         await raider.send_message(kek, "Chut ke dhakkan- Pussy lid")
         await raider.send_message(kek, "Chut ke gulam")
-        await raider.send_message(kek, "Chutiya ka bheja ghas khane gaya hai- idiot’s brain has gone to eat grass")
+        await raider.send_message(
+            kek,
+            "Chutiya ka bheja ghas khane gaya hai- idiot’s brain has gone to eat grass",
+        )
         await raider.send_message(kek, "Choot marani ka- Pussy whipped")
         await raider.send_message(kek, "Choot ka baal")
         await raider.send_message(kek, "Chipkali ke jhaat ke baal")
@@ -69,7 +101,9 @@ async def d3vilraid(event):
         await raider.send_message(kek, "Gaand main keera")
         await raider.send_message(kek, "Gaand mein bambu")
         await raider.send_message(kek, "Gaandfat")
-        await raider.send_message(kek, "Pote kitne bhi bade ho, lund ke niche hi rehte hai")
+        await raider.send_message(
+            kek, "Pote kitne bhi bade ho, lund ke niche hi rehte hai"
+        )
         await raider.send_message(kek, "Hazaar lund teri gaand main")
         await raider.send_message(kek, "Jhaant ke pissu-")
         await raider.send_message(kek, "Kali Choot Ke Safaid Jhaat")
@@ -106,5 +140,3 @@ async def d3vilraid(event):
         await raider.send_message(kek, "TU CHUTIYA TERA KHANDAAN CHUTIYA")
         await raider.send_message(kek, "AUR KITNA BOLU BEY MANN BHAR GAYA MERA🤣")
         await raider.send_message(kek, "Ab nikal ja jaake chkko k saath hilaa")
-        
-        
